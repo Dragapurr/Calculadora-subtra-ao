4a6b8ab9aeae1afe47c985cc6416a4ef9459bd48
[gab] <[gabestudos2205@gmail.com]>
2026-02-24 20:14:51 -0300

    esse é bom

---
+++ CalculadoraSubtracao.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class CalculadoraSubtracao {
+    public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    System.out.print("==calculadora subtracao==");
+System.out.print(" digite o primeiro numero: ");
+double numero1 = scanner.nextDouble();
+ System.out.print("Digite o segundo número: ");
+        double numero2 = scanner.nextDouble();
+
+        double resultado = numero1 - numero2;
+
+        System.out.println("Resultado: " + resultado);
+
+        scanner.close();
+
+
+    }
+}
